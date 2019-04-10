1d3a85a2367f42074c08227d38103c48e3176ff5
Mark Buckner <buckner.a.mark@gmail.com>
2019-04-10 11:45:04 -0600

    Update thing.js

--- thing.js
+++ thing.js
@@ -1,18 +1,11 @@
- var thing = {
-  "menu": {
-    "id": null,
-    "value": "File",
-    "popup": {
-       "menuitem": [
+ const menuItems [
          {"value": "New", "onclick": "CreateNewDoc()"},
          {"value": "Open", "onclick": "OpenDoc()"},
          {"value": "Close", "onclick": "CloseDoc()"}
        ]
-      }
-    }
-  }
-
-//write a function that iterates over the menuItems array in the object "thing" and returns the value property lower-cased
-
-//write a function that accepts an object, and if the menu.id in the object is null, it returns "we need an id for this object,"
-//else, it returns the id number. You'll need to create a second thing object (call it something else) to test this.
+      
+    
+//write a function accepts an array of objects that each contain a 'value' field. The function will 
+//will return a new array with the field 'value' lower-cased
+ 
+ //Use menuItem as an example
